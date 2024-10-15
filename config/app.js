47b73adb017890c7386f1736a9c53ecdf32fcae6
vibhav045 <155750223+vibhav045@users.noreply.github.com>
2024-10-15 14:42:34 +0530

    Create app.js

---
+++ config/app.js
@@ -0,0 +1,19 @@
+// app.js
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
